e91e9e7c7f7dd94df033739a03db87b05395e2f5
alegosto01 <ale.gostoli@gmail.com>
2025-02-07 12:23:26 +0100

    many updates

--- image_check.py
+++ image_check.py
@@ -9,7 +9,7 @@ from scipy.stats import skew, kurtosis
 
 
 # Load model and processor
-model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32"    )
 processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
 
